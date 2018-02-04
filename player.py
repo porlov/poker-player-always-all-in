ba27941334fe12b4e829c84e279d1399533f7164
Pavel Orlov <porlov@thumbtack.net>
2018-02-04 12:55:50 +0600

    All-in bet

--- player.py
+++ player.py
@@ -1,10 +1,12 @@
-
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "Version_01"
 
     def betRequest(self, game_state):
-        return 0
+        my_player_index = game_state['in_action']
+        my_player = game_state['players'][my_player_index]
+        my_stack = my_player['stack']
+
+        return my_stack
 
     def showdown(self, game_state):
         pass
-
